ae8cf7a6584ba94a8887b6756d3faa606ccbc74d
Christin Sibi <christinpsibi333@gmail.com>
2024-04-30 09:32:41 +0530

    Styles Updated

--- sigma/src/pages/page1/page1.jsx
+++ sigma/src/pages/page1/page1.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Footer from '../../components/footer/footer'
 
+
 export default function Page1() {
     return (
         <div className="main-container">
@@ -12,7 +13,7 @@ export default function Page1() {
             <div className="content">
                 <div className="content1">
                     <div className="image">
-                        <img src="Images/Asset 5 1.png" alt="" />
+                        <img src="Images\🦆 illustration _Logo design design brand branding_.png" alt="" />
                     </div>
 
                     <div className="description">
